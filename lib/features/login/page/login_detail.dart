64dcef670c836f04218dd6299dc9662594c48f36
jovidon <xalilovjovidon777@gmail.com>
2025-04-04 18:27:07 +0500

    some changes

--- lib/features/login/page/login_detail.dart
+++ lib/features/login/page/login_detail.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_app/core/utils/app_colors.dart';
+import 'package:store_app/features/login/widget/text_form_field.dart';
 
 class LoginDetail extends StatelessWidget {
   const LoginDetail({super.key});
@@ -6,8 +9,34 @@ class LoginDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(padding: EdgeInsets.only(top: 10)),
+      backgroundColor: AppColors.white,
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: 59,
+          left: 24,
+          right: 24,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Login to your account",
+              style: TextStyle(color: AppColors.black, fontSize: 32.sp),
+            ),
+            SizedBox(height: 8.h),
+            Text(
+              "It’s great to see you again.",
+              style: TextStyle(color: AppColors.hintText, fontSize: 16.sp),
+            ),
+            SizedBox(height: 24.h),
+            Text(
+              "Email",
+              style: TextStyle(color: AppColors.black, fontSize: 16.sp),
+            ),
+            StoreAuthTextField(label: "", hint: "hint", validator: (value) => null, controller: )
+          ],
+        ),
+      ),
     );
   }
 }
