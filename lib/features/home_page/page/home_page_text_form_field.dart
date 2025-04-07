5fb0494f916b6f6539ee1d792173f2c801fbe7b7
Clevrem <clevrem2@gmail.com>
2025-04-07 20:16:40 +0500

    Search qismi tayyor

--- lib/features/home_page/page/home_page_text_form_field.dart
+++ lib/features/home_page/page/home_page_text_form_field.dart
@@ -1,83 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:store_app/core/utils/app_colors.dart';
-
-// class HomePageTextFormField extends StatelessWidget {
-//   const HomePageTextFormField({
-//     super.key,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Expanded(
-//       child: SizedBox(
-//         width: double.infinity,
-//         height: 52.h,
-//         child: TextFormField(
-//           style: TextStyle(
-//             color: AppColors.black,
-//             fontSize: 18,
-//           ),
-//           decoration: InputDecoration(
-//             hintText: "Search for clothes...",
-//             hintStyle: TextStyle(color: AppColors.hintText, fontSize: 16),
-//             filled: true,
-//             fillColor: AppColors.white,
-//             suffixIcon: Padding(
-//               padding: EdgeInsets.all(12),
-//               child: SvgPicture.asset(
-//                 "assets/icons/microphone.svg",
-//                 color: AppColors.hintText,
-//               ),
-//             ),
-//             prefixIcon: Padding(
-//               padding: EdgeInsets.all(12),
-//               child: SvgPicture.asset(
-//                 "assets/icons/search.svg",
-//                 color: AppColors.hintText,
-//               ),
-//             ),
-//             enabledBorder: OutlineInputBorder(
-//               borderRadius: BorderRadius.circular(10),
-//               borderSide: BorderSide(
-//                 color: AppColors.buttonBorder,
-//                 width: 2,
-//               ),
-//             ),
-//             focusedBorder: OutlineInputBorder(
-//               borderRadius: BorderRadius.circular(12),
-//               borderSide: BorderSide(
-//                 color: AppColors.buttonBorder,
-//                 width: 2,
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
-
-import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class HomePageTextFormField extends StatefulWidget {
-  const HomePageTextFormField({super.key});
+  final double width;
+  final double height;
+  final TextEditingController controller;
+  final ValueChanged<String>? onChanged;
+
+  const HomePageTextFormField({
+    super.key,
+    this.width = 280,
+    this.height = 52,
+    required this.controller,
+    this.onChanged,
+  });
 
   @override
-  _VoiceSearchFieldBodyState createState() => _VoiceSearchFieldBodyState();
+  State<HomePageTextFormField> createState() => _HomePageTextFormFieldState();
 }
 
-class _VoiceSearchFieldBodyState extends State<HomePageTextFormField> {
+class _HomePageTextFormFieldState extends State<HomePageTextFormField> {
   late stt.SpeechToText _speech;
   bool _isListening = false;
-  final TextEditingController _controller = TextEditingController();
 
   @override
   void initState() {
@@ -85,56 +31,43 @@ class _VoiceSearchFieldBodyState extends State<HomePageTextFormField> {
     _speech = stt.SpeechToText();
   }
 
-  // Mikrofon ruxsatlarini so'rash
   Future<void> requestMicrophonePermission() async {
     final status = await Permission.microphone.request();
-
     if (status.isGranted) {
-      print('Microphone permission granted');
-    } else if (status.isDenied) {
-      print('Microphone permission denied');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Microphone permission is denied')),
-      );
+      _toggleListening();
     } else if (status.isPermanentlyDenied) {
-      print('Microphone permission is permanently denied');
       openAppSettings();
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Microphone permission is denied')),
+      );
     }
   }
 
-  // Tinglashni boshlash
   void _startListening() async {
     bool available = await _speech.initialize();
     if (available) {
       setState(() => _isListening = true);
       _speech.listen(
         onResult: (val) {
-          setState(() {
-            _controller.text = val.recognizedWords;
-            _controller.selection = TextSelection.fromPosition(
-              TextPosition(offset: _controller.text.length),
-            );
-          });
+          widget.controller.text = val.recognizedWords;
+          widget.controller.selection = TextSelection.fromPosition(
+            TextPosition(offset: widget.controller.text.length),
+          );
+          widget.onChanged?.call(val.recognizedWords); // 👈 notify parent
         },
-        listenFor: Duration(seconds: 30),
-        pauseFor: Duration(seconds: 5),
+        listenFor: const Duration(seconds: 30),
+        pauseFor: const Duration(seconds: 5),
         partialResults: true,
-      );
-    } else {
-      print("Speech recognition not available or initialization failed.");
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Speech recognition is not available")),
       );
     }
   }
 
-  // Tinglashni to'xtatish
   void _stopListening() {
     setState(() => _isListening = false);
     _speech.stop();
   }
 
-  // Tinglashni almashtirish
   void _toggleListening() {
     if (_isListening) {
       _stopListening();
@@ -146,13 +79,15 @@ class _VoiceSearchFieldBodyState extends State<HomePageTextFormField> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 280,
-      height: 52,
+      width: widget.width,
+      height: widget.height,
       child: TextFormField(
-        controller: _controller,
+        controller: widget.controller,
+        onChanged: widget.onChanged, // 👈 handle input change
         style: const TextStyle(
           color: Colors.black,
-          fontSize: 18,
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
         ),
         decoration: InputDecoration(
           hintText: "Search for clothes...",
@@ -160,10 +95,7 @@ class _VoiceSearchFieldBodyState extends State<HomePageTextFormField> {
           filled: true,
           fillColor: Colors.white,
           suffixIcon: GestureDetector(
-            onTap: () async {
-              await requestMicrophonePermission();
-              _toggleListening();
-            },
+            onTap: () async => await requestMicrophonePermission(),
             child: Padding(
               padding: const EdgeInsets.all(12),
               child: SvgPicture.asset(
@@ -198,97 +130,3 @@ class _VoiceSearchFieldBodyState extends State<HomePageTextFormField> {
     );
   }
 }
-
-
-
-// class _VoiceSearchFieldBodyState extends State<_VoiceSearchFieldBody> {
-//   late stt.SpeechToText _speech;
-//   bool _isListening = false;
-//   final TextEditingController _controller = TextEditingController();
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _speech = stt.SpeechToText();
-//   }
-//
-//   void _startListening() async {
-//     bool available = await _speech.initialize();
-//     if (available) {
-//       setState(() => _isListening = true);
-//       _speech.listen(onResult: (val) {
-//         setState(() {
-//           _controller.text = val.recognizedWords;
-//           _controller.selection = TextSelection.fromPosition(
-//             TextPosition(offset: _controller.text.length),
-//           );
-//         });
-//       });
-//     }
-//   }
-//
-//   void _stopListening() {
-//     setState(() => _isListening = false);
-//     _speech.stop();
-//   }
-//
-//   void _toggleListening() {
-//     if (_isListening) {
-//       _stopListening();
-//     } else {
-//       _startListening();
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return SizedBox(
-//       width: 280,
-//       height: 52,
-//       child: TextFormField(
-//         controller: _controller,
-//         style: const TextStyle(
-//           color: Colors.black,
-//           fontSize: 18,
-//         ),
-//         decoration: InputDecoration(
-//           hintText: "Search for clothes...",
-//           hintStyle: const TextStyle(color: Colors.grey, fontSize: 16),
-//           filled: true,
-//           fillColor: Colors.white,
-//           suffixIcon: GestureDetector(
-//             onTap: _toggleListening,
-//             child: Padding(
-//               padding: const EdgeInsets.all(12),
-//               child: SvgPicture.asset(
-//                 "assets/icons/microphone.svg",
-//                 color: _isListening ? Colors.red : Colors.grey,
-//               ),
-//             ),
-//           ),
-//           prefixIcon: Padding(
-//             padding: const EdgeInsets.all(12),
-//             child: SvgPicture.asset(
-//               "assets/icons/search.svg",
-//               color: Colors.grey,
-//             ),
-//           ),
-//           enabledBorder: OutlineInputBorder(
-//             borderRadius: BorderRadius.circular(10),
-//             borderSide: const BorderSide(
-//               color: Colors.black12,
-//               width: 2,
-//             ),
-//           ),
-//           focusedBorder: OutlineInputBorder(
-//             borderRadius: BorderRadius.circular(12),
-//             borderSide: const BorderSide(
-//               color: Colors.black26,
-//               width: 2,
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
