e10f5f40e88306697c71ac015606fac666aa0195
Clevrem <clevrem2@gmail.com>
2025-04-09 13:51:59 +0500

    some Changes on Custom Exceptions

--- lib/Core/exceptions/cutom_exception.dart
+++
@@ -1,9 +0,0 @@
-class CustomException implements Exception {
-  final String errorMessage;
-  final int statusCode;
-
-  CustomException({
-    required this.errorMessage,
-    required this.statusCode,
-  });
-}
--- lib/Core/navigation/router.dart
+++ lib/Core/navigation/router.dart
@@ -21,10 +21,7 @@ import '../../Features/Auth/store_onboarding/page/state_oboarding_detail.dart';
 import '../../Features/notification_page/page/notification.dart';
 
 final router = GoRouter(
-
-
   initialLocation: Routes.login,
- 
   routes: [
     GoRoute(
       path: Routes.onboarding,
