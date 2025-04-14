f2fed3bc273b29f47b7200fc0dec3e907a027981
jovidon <xalilovjovidon777@gmail.com>
2025-04-14 14:02:17 +0500

    map qismini davomi

--- lib/Features/Auth/store_onboarding/page/store_app_elevated_button.dart
+++ lib/Features/Auth/store_onboarding/page/store_app_elevated_button.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
-import 'package:store_app/Core/navigation/routes.dart';
 
 class StoreAppElevatedButton extends StatelessWidget {
   const StoreAppElevatedButton({
