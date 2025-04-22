0a5c9910862112fc29eb601a4a18c02e8ea7c2e2
Clevrem <clevrem2@gmail.com>
2025-04-22 14:34:36 +0500

    details page

--- lib/Features/details/page/details.dart
+++ lib/Features/details/page/details.dart
@@ -55,7 +55,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                           ),
                         ),
                       ),
-                      Positioned(
+                      Positioned(//
                         top: 8,
                         right: 7,
                         child: LikeButton<DetailsModel>(
